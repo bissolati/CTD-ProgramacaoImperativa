120effcbcb48f18e9fd998340b746507cab1993c
bissolati <bissolatimarcelo@gmail.com>
2021-09-05 17:07:53 -0300

    Exercícios Números Pares While Aula 11

---
+++ ex-while-numeros-pares.js
@@ -0,0 +1,9 @@
+/* Escreva um aplicativo em Javascript que mostre todos os números
+pares de 1 até 100.*/
+let i = 1;
+while (i <= 100) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    i++;
+}
