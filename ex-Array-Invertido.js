a4f0b1a96d563a5d605fe2295c6255900261879d
bissolati <bissolatimarcelo@gmail.com>
2021-09-04 22:20:10 -0300

    Exercício Array Invertido Aula 10

---
+++ ex-Array-Invertido.js
@@ -0,0 +1,32 @@
+/*Crie a função imprimirInverso que pega um Array como um argumento e
+imprime cada elemento em ordem reversa no console (você não precisa
+inverter o Array).*/
+var lista = ['Maça', 'Banana', 'Abacaxi', 'Laranja']
+
+function imprimirInverso(Listas) {
+    console.log(3, Listas[3]);
+    console.log(2, Listas[2]);
+    console.log(1, Listas[1]);
+    console.log(0, Listas[0]);
+}
+
+console.log("Lista invertida manual: ");
+imprimirInverso(lista);
+
+console.log("------------------------------------------");
+/* 2. Crie a função inverter que recebe um Array como argumento e retorna um novo invertido.*/
+var lista = ['Maça', 'Banana', 'Abacaxi', 'Laranja']
+
+function Inverso(Array) {
+    return console.log(Array.reverse())
+}
+
+console.log("Lista normal: " );
+console.log(lista);
+console.log();
+console.log("Lista invertida");
+Inverso(lista);
+
+console.log();
+console.log("-------------------------------------");
+console.log();
